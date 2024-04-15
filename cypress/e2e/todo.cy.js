aa992faa51a010c726a36a33f892dc6b56a37bff
Manuel González Gómez <tete.manolo95@gmail.com>
2024-04-15 12:34:42 +0200

    verificando

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -4,12 +4,15 @@ describe('agregar tareas', () => {
         cy.visit('https://todomvc.com/examples/react/dist/')
         cy.get(' .new-todo').type('tarea 1{enter}')
         cy.get('[data-testid="todo-item-label"]').contains("tarea 1")
+        cy.contains('tarea 1').should('be.visible')
     })
     it('marcar tarea como completada', () => {
         cy.visit('https://todomvc.com/examples/react/dist/')
         cy.get(' .new-todo').type('tarea 1{enter}')
         cy.get('[data-testid="todo-item-label"]').contains("tarea 1")
         cy.get('[data-testid="todo-item-toggle"]').click()
+        cy.get('[data-testid="todo-item-label"]').should('be.visible')
+        
     })
         it('desmarcar tarea completada', () => {
             cy.visit('https://todomvc.com/examples/react/dist/')
@@ -17,6 +20,7 @@ describe('agregar tareas', () => {
             cy.get('[data-testid="todo-item-label"]').contains("tarea 1")
             cy.get('[data-testid="todo-item-toggle"]').click()
             cy.get('[data-testid="todo-item-toggle"]').click()
+            cy.get('[data-testid="todo-item-label"]').should('be.visible')
         })
             it('editar tarea', () => {
                 cy.visit('https://todomvc.com/examples/react/dist/')
@@ -24,6 +28,7 @@ describe('agregar tareas', () => {
                 cy.get('[data-testid="todo-item-label"]').contains("tarea 1")
                 cy.get('[data-testid="todo-item-label"]').dblclick()
                 cy.focused().clear().type("tarea 2{enter}")
+                cy.get('[data-testid="todo-item-label"]').should('be.visible')
             })
             it('borrar una tarea', () => {
                 cy.visit('https://todomvc.com/examples/react/dist/')
@@ -31,6 +36,7 @@ describe('agregar tareas', () => {
                 cy.get('[data-testid="todo-item-label"]').contains("tarea 1")
                 cy.get('button').invoke('show')
                 cy.get('[data-testid="todo-item-button"]').click()
+                cy.get('[data-testid="todo-item-label"]').should('not.exist')
             })
 
             it('filtrar tareas', () => {
@@ -41,9 +47,13 @@ describe('agregar tareas', () => {
                 cy.get(' .new-todo').type('tarea 2{enter}')
                 cy.get(' .new-todo').type('tarea 3{enter}')
                 cy.get('[data-testid="footer-navigation"] > :nth-child(3) > a').click()
+                cy.get('[data-testid="todo-item-label"]').should('be.visible')
                 cy.get(':nth-child(2) > a').click()
+                cy.get(':nth-child(1) > .view > [data-testid="todo-item-label"]').should('be.visible')
+                cy.get(':nth-child(2) > .view > [data-testid="todo-item-label"]').should('be.visible')
                 cy.get(':nth-child(1) > a').click()
+                cy.get('body').should('be.visible')
 
-
+                
     })
 })
